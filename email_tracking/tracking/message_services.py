33df454cf7dad3cc4f81fb33aec1a9c90ac13314
Akshay Babar <akshaybabar2408@gmail.com>
2022-05-19 11:45:44 +0530

    create script and view

--- email_tracking/tracking/message_services.py
+++ email_tracking/tracking/message_services.py
@@ -1,9 +1,9 @@
 import requests
+import random
 
-print("adfasdfs")
+msg_type_list = ["message.created", "message.replied", "message.bounced"]
 
-api_url = "http://localhost:9000/event"
-param = {"keyss": "asdfasdf"}
-res = requests.post(api_url, data=param)
-
-print(res)
+api_url = "http://localhost:8000/event"
+for i in range(1, 20):
+    param = {"type": random.choice(msg_type_list)}  # randomly choose the type
+    res = requests.post(api_url, data=param)
--- email_tracking/tracking/views.py
+++ email_tracking/tracking/views.py
@@ -1,13 +1,53 @@
 from django.http import JsonResponse
-from django.shortcuts import render
 from django.views.decorators.csrf import csrf_exempt
+import datetime
+import string
+import random
 
 
-# Create your views here.
+def createRandomDateTimestamp():
+    start_date = datetime.date(1990, 1, 1)
+    end_date = datetime.date(2022, 1, 1)
+
+    time_between_dates = end_date - start_date
+    days_between_dates = time_between_dates.days
+    random_number_of_days = random.randrange(days_between_dates)
+    random_date = start_date + datetime.timedelta(days=random_number_of_days)
+    # generate timestamp
+    return random_date.strftime('%s')
 
 
 @csrf_exempt
 def eventAPI(request):
-    import pdb
-    pdb.set_trace()
-    return JsonResponse({"is_payment_pending": True})
+    # msg match id
+    matchid = random.randint(1, 10000)
+
+    # generate alphanumeric string
+    S = 25  # number of char
+    msgId = ''.join(random.choices(string.ascii_lowercase + string.digits, k=S))
+
+    # generate random timestamp
+    timestamp = createRandomDateTimestamp()
+    custom_msg = {}
+    if request.method == 'POST':
+        msgType = request.POST['type']
+
+        # custom
+        metadata = {
+            "message_id": msgId
+        }
+
+        custom_msg = {
+            "date": timestamp,
+            "type": msgType,
+            "metadata": metadata
+        }
+
+        if msgType == "message.created":
+            metadata['match_id'] = matchid
+        elif msgType == "message.replied":
+            metadata['match_id'] = matchid
+        else:   # message.bounced
+            pass
+
+    return JsonResponse({"message": custom_msg})
